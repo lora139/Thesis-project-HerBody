7a75a0023e10c64c05aa974bb4b457032e50cab7
lora139 <loloio.dim@gmail.com>
2022-11-27 14:18:56 +0200

    about and product wight, height px

---
+++ deleteprod.php
@@ -0,0 +1,14 @@
+<?php
+
+require "global.php";
+global $con;
+// take the parameters form the url address
+$token = isset($_GET['token']) ? $_GET['token'] : null;
+
+//deleting the item with id = token (url parameter)
+$sql = "Delete from products where `id`= '$token'";
+$res = mysqli_query($con, $sql);
+
+//main page
+header('Location: index.php#product')
+?>
--- index.php
+++ index.php
@@ -4,60 +4,61 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-control" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HERBODY</title>
-    <link rel="stylesheet" href="/css/style.css"> <!-- adding css file -->
-   
-</head>
-<body>
-<header>
-    <a href="#" class="logo">Herbody<span>.</span></a>
-    <div class="menuToggle" onclick="toggleMenu();"></div>
-    <ul class="navigation">
-        <li><a href="#banner" onclick="toggleMenu();">Home</a></li>
-        <li><a href="#about" onclick="toggleMenu();">About</a></li>
-        <li><a href="#product" onclick="toggleMenu();">Products</a></li>
-        <!--<li><a href="#testemonials">Testemonials</a></li> -->
-        <li><a href="#contact" onclick="toggleMenu();">Contact</a></li>
-        
-        <?php
-        require "global.php";
+    <title>HERBODY</title> <!-- Името на страница -->
+    <link rel="stylesheet" href="css/style.css"> <!-- добавяне на css file -->
 
-        global $con;
-        /* if we are logged in instead of displaying a link to the login page, display a dropdown menu for the client */
-        if (isset($_COOKIE["login"])) {
-            // dropdown menu
-            /*<div class="dropdown">
-                <button class="dropbtn">Profile</button>
-                <div class="dropdown-content">
-                    <a href="profile.php" onclick="toggleMenu();">Profile</a>
-                    <a href="logout.php" onclick="toggleMenu();">Logout</a>
-                </div>
-            </div>
-            */
-            $sql = "select is_admin from `user` where username = '". $_COOKIE["login"] . "'";
-            $res = mysqli_query($con, $sql);
-            $row = mysqli_fetch_row($res);
-            
-            if($row[0] == 1){
-               echo "<div class=\"dropdown\"><li><a class=\"dropbtn\">Profile</a><div class=\"dropdown-content\"><a href=\"profile.php\" onclick=\"toggleMenu();\">Client accounts</a><a href=\"logout.php\" onclick=\"toggleMenu();\">Logout</a></div></li></div>";
-            }else{
-               echo "<div class=\"dropdown\"><li><a class=\"dropbtn\">Profile</a><div class=\"dropdown-content\"><a href=\"logout.php\" onclick=\"toggleMenu();\">Logout</a></div></li></div>";
-            }
-        } else {
-            echo "<li><a href=\"login.php\" onclick=\"toggleMenu();\">Log in</a></li>";
-        }
-        ?>
-        
-    </ul>
-</header>
-<section class="banner" id="banner">
+</head>
+
+<body>
+    <header>
+        <a href="#" class="logo">Herbody<span>.</span></a> <!-- Логото -->
+        <div class="menuToggle" onclick="toggleMenu();"></div><!--тук ли трябва да е div-а-->
+            <ul class="navigation"> <!--Полето с навигацията и менютата-->
+                <li><a href="#banner" onclick="toggleMenu();">Home</a></li> <!--Home полето-->
+                <li><a href="#about" onclick="toggleMenu();">About</a></li> <!--About полето -->
+                <li><a href="#product" onclick="toggleMenu();">Products</a></li><!--Products полето-->
+                <li><a href="#contact" onclick="toggleMenu();">Contact</a></li><!--Contact полето-->
+                <!-- <li><a href="/newprod.php">Newprod</a></li>Contact полето -->
+                    <?php
+                        require "global.php";
+
+                        global $con;
+                        /* if we are logged in instead of displaying a link to the login page, display a dropdown menu for the client */
+                        if (isset($_COOKIE["login"])) {
+                            // dropdown menu
+                            /*<div class="dropdown">
+                             <button class="dropbtn">Profile</button>
+                            <div class="dropdown-content">
+                            <a href="profile.php" onclick="toggleMenu();">Profile</a>
+                            <a href="logout.php" onclick="toggleMenu();">Logout</a>
+                            </div>
+                            </div>
+                            */
+                            $sql = "select is_admin from `user` where username = '". $_COOKIE["login"] . "'";
+                            $res = mysqli_query($con, $sql);
+                            $row = mysqli_fetch_row($res);
+
+                            if($row[0] == 1){
+                                echo "<li><a href=\"/newprod.php\">Add product</a></li>";
+                                echo "<div class=\"dropdown\"><li><a class=\"dropbtn\">Profile</a><div class=\"dropdown-content\"><a href=\"profile.php\" onclick=\"toggleMenu();\">Client accounts</a><a href=\"logout.php\" onclick=\"toggleMenu();\">Logout</a></div></li></div>";
+                            }else{
+                                echo "<div class=\"dropdown\"><li><a class=\"dropbtn\">Profile</a><div class=\"dropdown-content\"><a href=\"logout.php\" onclick=\"toggleMenu();\">Logout</a></div></li></div>";
+                             }
+                        } else {
+                             echo "<li><a href=\"login.php\" onclick=\"toggleMenu();\">Log in</a></li>";
+                        }
+                    ?>
+            </ul>
+    </header>
+    <section class="banner" id="banner">
     <div class="content">
         <h2>Always choose good</h2>
         <p>Handmade soaps with products from nature</p>
         <a href="#product" class="btn">Products</a>
     </div>
-</section>
+    </section>
 <!-- <div class ="parallax"></div> -->
 <section class="about" id="about">
     <div class="row">
@@ -67,25 +68,25 @@
             <br> Lorem Ipsum is simply dummy
             text of the printing and typesetting industry. Lorem Ipsum has been
             the industry's standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a 
+            unknown printer took a galley of type and scrambled it to make a
             type specimen book.<br><br>Lorem Ipsum is simply dummy
             text of the printing and typesetting industry. Lorem Ipsum has been
             the industry's standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a 
+            unknown printer took a galley of type and scrambled it to make a
             type specimen book.Lorem Ipsum is simply dummy
             text of the printing and typesetting industry. Lorem Ipsum has been
             the industry's standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a 
+            unknown printer took a galley of type and scrambled it to make a
             type specimen book.
-            inter took a galley of type and scrambled it to make a 
+            inter took a galley of type and scrambled it to make a
             type specimen book.Lorem Ipsum is simply dummy
             text of the printing and typesetting industry. Lorem Ipsum has been
             the industry's standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a 
+            unknown printer took a galley of type and scrambled it to make a
             type specimen book.
             </p>
         </div>
-        
+
         <div class="col50">
             <div class="box">
             <div class="imgBox">
@@ -113,7 +114,7 @@
                 $n = 0;
                 /* select all the products which we import form the DB */
                 $sql = "SELECT * FROM `products` ORDER BY id";
-                /* return the result */        
+                /* return the result */
                 $res = mysqli_query($con, $sql);
                 /* if it is successful, it puts all the product into an array*/
                 if ($res) {
@@ -124,12 +125,21 @@
                     /* free result from memory */
                     mysqli_free_result($res);
                 } else return;  //returns nothing
-
+                    
+                // $pages = ceil($n / 3);
+                // for ($i = 0; $i < $pages; ++$i) {
+                //     $page_html = "<div class='triple-box'>";
+                //     for ($j = 3 * $i; $j < 3; ++$j) {
+                //         $page_html += "<div class='box'><div class="
+                //     }
+                //     $page_html += "</div>";
+                // }
+                
                 /*generate html for products page*/
                 for ($i = 0; $i < $n; ++$i) {
                     /* special case when we reach the final product card */
                     if ($i == $n - 1) {
-                        echo "<div class='box'><div class='imgBox'><img src='".$prod[$i][1]."'></div><div class='text'><h3>".$prod[$i][2]."</h3><p class='price'>".$prod[$i][4]."$ ".$prod[$i][3]."</p><div class='information'><p>".$prod[$i][5]."</p></div><p><button>Learn more</button></p></div></div>";    
+                        echo "<div class='box'><div class='imgBox'><img src='".$prod[$i][1]."'></div><div class='text'><h3>".$prod[$i][2]."</h3><p class='price'>".$prod[$i][4]."$ ".$prod[$i][3]."</p><div class='information'><p>".$prod[$i][5]."</p></div><p><button>Learn more</button></p></div></div>";
                         echo "</div>";
                         break;
                     }
@@ -139,16 +149,31 @@
                     if ($i % 3 == 0) echo "<div class='triple-box'>";
                     /* write html code for product card */
                     echo "<div class='box'><div class='imgBox'><img src='".$prod[$i][1]."'></div><div class='text'><h3>".$prod[$i][2]."</h3><p class='price'>".$prod[$i][4]."$ ".$prod[$i][3]."</p><div class='information'><p>".$prod[$i][5]."</p></div>";
-                    if (isset($_COOKIE["login"]) && $_COOKIE["login"] == "1") {
-                        echo "<p><button onclick=\"location.href='/product.php?id=".$prod[$i][0]."';\">Learn more</button></p></div></div>";
+
+
+                    if(isset($_COOKIE["login"])){
+                        // echo "<p><button onclick=\"location.href='/product.php?id=".$prod[$i][0]."';\">Learn more</button></p></div></div>";
+
+                        $sql = "select is_admin from `user` where username = '". $_COOKIE["login"] . "'";
+                        $res = mysqli_query($con, $sql);
+                        $row = mysqli_fetch_row($res);
+
+                        if($row[0]==1){
+                            echo "<p><button onclick=\"location.href='/product.php?id=".$prod[$i][0]."';\">Learn more</button></p>";
+                            echo "<p><button onclick=\"location.href='/newprod.php?token=".$prod[$i][0]."';\">Edit</button></p>";
+                            echo "<p><button onclick=\"location.href='/deleteprod.php?token=".$prod[$i][0]."';\">Delete</button></p></div></div>";
+                        }else{
+                            echo "<p><button onclick=\"location.href='/product.php?id=".$prod[$i][0]."';\">Learn more</button></p></div></div>";
+                        }
                     } else {
                         echo "<p><button onclick=\"location.href='/login.php';\">Learn more</button></p></div></div>";
+
                     }
                 }
             }
             get_products();
             ?>
-            
+
         </div>
         <center>
             <button class="btn" onclick="slider_shift(-1)">&#10094;</button>
@@ -156,7 +181,6 @@
         </center>
     </div>
 
-   
 </section>
 
 <section class="contact" id="contact">
