260c9c42adbe6b279f1fb9877b3bf1e2943dd30b
lora139 <loloio.dim@gmail.com>
2023-03-15 09:51:36 +0200

    format

--- deleteprod.php
+++ deleteprod.php
@@ -1,14 +1,13 @@
 <?php
+    require "global.php";
+    global $con;
+    // take the parameters form the url address
+    $token = isset($_GET['token']) ? $_GET['token'] : null;
 
-require "global.php";
-global $con;
-// take the parameters form the url address
-$token = isset($_GET['token']) ? $_GET['token'] : null;
+    //deleting the item with id = token (url parameter)
+    $sql = "Delete from products where `id`= '$token'";
+    $res = mysqli_query($con, $sql);
 
-//deleting the item with id = token (url parameter)
-$sql = "Delete from products where `id`= '$token'";
-$res = mysqli_query($con, $sql);
-
-//main page
-header('Location: index.php#product')
+    //main page
+    header('Location: index.php#product')
 ?>
